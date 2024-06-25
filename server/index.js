88d11eb54798cc6380cc233227459cde48a48e65
duynghiadev <duynghia22302@gmail.com>
2024-06-25 14:43:56 +0700

    create database models

--- server/index.js
+++ server/index.js
@@ -1,4 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://duynghia22302:ErE4Q9rIs2zIwzs4@mern-learnit.fkuzydx.mongodb.net/?appName=mern-learnit`,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+      }
+    );
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 const app = express();
 
