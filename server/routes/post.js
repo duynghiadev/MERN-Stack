770f65da5feb79f74bbe808502615a7c11933aad
duynghiadev <duynghia22302@gmail.com>
2024-06-25 18:34:01 +0700

    feat: add feature PUT data to database

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,10 +4,24 @@ const verifyToken = require("../middleware/auth");
 
 const Post = require("../models/Post");
 
+// @route GET api/posts
+// @desc Get post
+// @access Private
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const posts = await Post.find({ user: req.userId }).populate("user", [
+      "username",
+    ]);
+    res.json({ success: true, posts });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
 // @route POST api/posts
 // @desc Create post
 // @access Private
-
 router.post("/", verifyToken, async (req, res) => {
   const { title, description, url, status } = req.body;
 
@@ -29,6 +43,55 @@ router.post("/", verifyToken, async (req, res) => {
     await newPost.save();
 
     res.json({ success: true, message: "Hapy learning!", post: newPost });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+// @route PUT api/posts
+// @desc Update post
+// @access Private
+router.put("/:id", verifyToken, async (req, res) => {
+  const { title, description, url, status } = req.body;
+
+  // Simple validation
+  if (!title)
+    return res
+      .status(400)
+      .json({ success: false, message: "Title is required" });
+
+  try {
+    let updatedPost = {
+      title,
+      description: description || "",
+      url: (url.startsWith("https://") ? url : `https://${url}`) || "",
+      status: status || "TO LEARN",
+    };
+
+    const postUpdateCondition = {
+      _id: req.params.id,
+      user: req.userId,
+    };
+
+    updatedPost = await Post.findOneAndUpdate(
+      postUpdateCondition,
+      updatedPost,
+      { new: true }
+    );
+
+    // User not authorised to update post
+    if (!updatedPost)
+      return res.status(401).json({
+        sucess: false,
+        message: "Post not found or user or user not authorised",
+      });
+
+    res.json({
+      success: true,
+      message: "Excellent progress",
+      post: updatedPost,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: "Internal server error" });
