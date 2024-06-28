cc7818328fe0c087707e9c3be8b8a4ce6bc402eb
duynghiadev <duynghia22302@gmail.com>
2024-06-28 22:26:05 +0700

    edit config vercel (#1)

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:5000/api"
-    : "https://example.herokuapp.com/api";
+    ? "https://mern-stack-learn-it-backend.vercel.app/api"
+    : "";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
 
